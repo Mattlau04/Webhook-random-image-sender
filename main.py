52e1dc3ac8f982bf5b7de028bcfbb4e6d2ff2df6
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2019-05-11 21:58:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def forever():
     print ("Sending images every " + str(interval) + " seconds.")
     while True:
         try:
-            file = random.choice(os.listdir(".\\images\\"))
+            file = random.choice(getListOfFiles('.\\images\\'))
         except Exception:
             print ("There are no files in the images folder. Please add some.")
             time.sleep(5)
             sys.exit()
         print (file)
-        file = File(".\\images\\" + str(file))
+        file = File(str(file))
         hook.send(file=file)
         time.sleep(int(interval))
 
@@ -29,15 +29,32 @@ def notforever():
     print ("Sending " + str(loop) + " images every " + str(interval) + " seconds.")
     for x in range(int(loop)):
         try:
-            file = random.choice(os.listdir(".\\images\\"))
+            file = random.choice(getListOfFiles('.\\images\\'))
         except Exception:
             print ("There are no files in the images folder. Please add some.")
             time.sleep(5)
             sys.exit()
         print (file)
-        file = File(".\\images\\" + str(file))
+        file = File(str(file))
         hook.send(file=file)
         time.sleep(int(interval))
+		
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
 
 if loop == "forever":
     forever()
